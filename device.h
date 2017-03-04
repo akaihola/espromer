9132fbe413b8604161207d565d6c9928e8e7aec5
Antti Kaihola <antti16+github@kaihola.fi>
2017-03-04 15:51:45 +0200

    Define all devices in one file. Included an example file.

--- device.h
+++ device.h
@@ -1,2 +1,9 @@
-#include "device_2.h"
+// Choose device configuration by changing the device number.
+// Add corresponding definitions to devices.h using:
+// #if DEVICE==<n>
+//   // definitions here
+// #endif
+
+#define DEVICE 1
+#include "devices.h"
 
---
+++ devices.h
@@ -0,0 +1,16 @@
+#if DEVICE==1
+  // Description of device #1
+  #define ONE_WIRE_BUS 2
+  #define REGULATOR_ENABLE_PIN 15
+  #define THINGSPEAK_API_KEY "(your ThingSpeak API write key)"
+  #define WIFI_SSID "(your WiFi SSID)"
+  #define WIFI_PASSWORD "(your WiFi password)"
+#endif
+
+#if DEVICE==2
+  // Description of device #2
+  #define ONE_WIRE_BUS 14
+  #define THINGSPEAK_API_KEY "(your ThingSpeak API write key)"
+  #define WIFI_SSID "(your WiFi SSID)"
+  #define WIFI_PASSWORD "(your WiFi password)"
+#endif
